e93fcc8b183f4cb0700a423c8bf19085b7688d1c
Kouhai <devkouhai@gmail.com>
2021-05-20 23:29:08 -0500

    aegisub-wangqr: Add missing hash

--- Casks/aegisub-wangqr.rb
+++ Casks/aegisub-wangqr.rb
@@ -1,13 +1,12 @@
 cask "aegisub-wangqr" do
-  # HACK: Actual version numbers are overrated. :REWdako:
   version "r9198,20210418"
 
-  # FIXME: Add sha256
   url "https://ftp.wangqr.tk/aegisub/aegisub-macos-#{version.after_comma}-#{version.before_comma}.dmg",
       verified: "ftp.wangqr.tk/aegisub/"
   name "Aegisub (wangqr)"
   desc "Wangqr Aegisub, #{version.before_comma}"
   homepage "https://github.com/wangqr/Aegisub"
+  sha256 "284643ba458a61f80fea138a7820015999bcfac11fbaf36de1b16a0ae590ed34"
 
   app "Aegisub.app"
 end
